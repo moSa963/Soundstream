420f626fd86b663226cbdca39a0f885657f7d5c0
M S <mo.sa963@hotmail.com>
2023-03-31 21:50:56 +0300

    Add Track Model

---
+++ app/Models/Track.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Track extends Model
+{
+    use HasFactory;
+
+    protected $table = "tracks";
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'location',
+        'duration',
+        'explicit',
+        'photo',
+        'written_by',
+        'performed_by',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function playlists(){
+        return $this->belongsToMany(Playlist::class, PlaylistTrack::class);
+    }
+
+    public function likes(){
+        return $this->hasMany(LikedTrack::class);
+    }
+}
