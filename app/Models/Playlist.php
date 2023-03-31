e41e00ae11e0d84f0c4717db3549cf9294928b63
M S <mo.sa963@hotmail.com>
2023-03-31 21:51:20 +0300

    Add Playlist Model

---
+++ app/Models/Playlist.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Playlist extends Model
+{
+    use HasFactory;
+    protected $table = "playlists";
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'photo',
+        'album',
+    ];
+
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function tracks(){
+        return $this->belongsToMany(Playlist::class, PlaylistTrack::class);
+    }
+}
