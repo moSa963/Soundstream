e3624f50de9fea380871387da1544c09386c38a2
M S <mo.sa963@hotmail.com>
2023-04-28 22:40:57 +0300

    Add auth middleware

--- routes/api.php
+++ routes/api.php
@@ -77,13 +77,13 @@ Route::controller(StreamTrackController::class)
 Route::controller(TrackPhotoController::class)
     ->group(function(){
         Route::get("tracks/{track}/photo", "index");
-        Route::post("tracks/{track}/photo", "update");
-        Route::delete("tracks/{track}/photo", "destroy");
+        Route::post("tracks/{track}/photo", "update")->middleware("auth:sanctum");
+        Route::delete("tracks/{track}/photo", "destroy")->middleware("auth:sanctum");
     });
 
 Route::controller(PlaylistPhotoController::class)
     ->group(function(){
         Route::get("playlists/{playlist}/photo", "index");
-        Route::post("playlists/{playlist}/photo", "update");
-        Route::delete("playlists/{playlist}/photo", "destroy");
+        Route::post("playlists/{playlist}/photo", "update")->middleware("auth:sanctum");
+        Route::delete("playlists/{playlist}/photo", "destroy")->middleware("auth:sanctum");
     });
