30fced7b7e4e95d05d67d9a57133bc5c8551b0d3
M S <mo.sa963@hotmail.com>
2023-05-05 21:02:50 +0300

    Add TrackHistory 'add' method to update history when user play a track

--- app/Http/Controllers/Api/StreamTrackController.php
+++ app/Http/Controllers/Api/StreamTrackController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Track;
+use App\Models\TrackHistory;
 use App\Services\StreamService;
 use Illuminate\Http\Request;
 
@@ -11,6 +12,8 @@ class StreamTrackController extends Controller
 {
     public function show(Request $request, Track $track)
     {
+        TrackHistory::add($request->user(), $track);
+        
         return StreamService::download("tracks/{$track->location}", $request->header("Range", null), $track->title);
     }
 }
