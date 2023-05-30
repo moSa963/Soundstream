46648c3976972991b670214b4ed8d0a734865641
M S <mo.sa963@hotmail.com>
2023-05-30 19:58:18 +0300

    Add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\LikedTrack;
+use App\Models\Playlist;
+use App\Models\PlaylistTrack;
+use App\Models\Track;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +18,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create([
+            'username' => 'admin',
+        ]);
+            
+        // admin has 10 tracks
+        Track::factory(10)->create([ "user_id" => $user->id ]);
+        
+        // admin liked 10 tracks
+        $tracks = LikedTrack::factory(10)->create([ "user_id" => $user->id ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $playlist = Playlist::factory()->create([ "user_id" => $user->id ]);
+
+        $album = Playlist::factory()->create([ "user_id" => $user->id, "album" => true ]);
+
+        foreach($tracks as $track)
+        {
+            PlaylistTrack::create([
+                "playlist_id" => $playlist->id,
+                "track_id" => $track->id,
+            ]);
+
+            PlaylistTrack::create([
+                "playlist_id" => $album->id,
+                "track_id" => $track->id,
+            ]);
+        }
     }
 }
