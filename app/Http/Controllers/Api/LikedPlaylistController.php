376c6fc5cc79b69c88c5f59613a4d14a6d20e182
M S <mo.sa963@hotmail.com>
2023-06-01 22:58:32 +0300

    Add liked playlists controller

---
+++ app/Http/Controllers/Api/LikedPlaylistController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\LikedPlaylist;
+use App\Models\Playlist;
+use Illuminate\Http\Request;
+
+class LikedPlaylistController extends Controller
+{
+    public function store(Request $request, Playlist $playlist)
+    {
+        LikedPlaylist::firstOrCreate([
+            'user_id' => $request->user()->id,
+            'playlist_id' => $playlist->id,
+        ]);
+
+        return response()->noContent();
+    }
+
+    public function destroy(Request $request, Playlist $playlist)
+    {
+        LikedPlaylist::where('user_id', $request->user()->id)
+            ->where('playlist_id', $playlist->id)
+            ->delete();
+
+        return response()->noContent();
+    }
+}
