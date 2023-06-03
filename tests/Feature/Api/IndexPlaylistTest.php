53f0957cee9a7e18978c4a8d4a096db778a0ed5b
M S <mo.sa963@hotmail.com>
2023-06-03 10:05:36 +0300

    Add test case to test that liked playlists are included in the response

--- tests/Feature/Api/IndexPlaylistTest.php
+++ tests/Feature/Api/IndexPlaylistTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\LikedPlaylist;
 use App\Models\Playlist;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -28,5 +29,22 @@ class IndexPlaylistTest extends TestCase
         $response->assertSuccessful();
 
         $response->assertJsonCount(5, "data");
+    }
+
+    public function test_user_can_get_a_list_of_his_playlists_with_liked_playlists(): void
+    {
+        $user = User::factory()->create();
+        
+        Playlist::factory(5)->create([ "user_id" => $user->id ]);
+        Playlist::factory()->create();
+        LikedPlaylist::factory(5)->create([ "user_id" => $user->id ]);
+        
+        Sanctum::actingAs($user);
+
+        $response = $this->get('api/playlists');
+
+        $response->assertSuccessful();
+
+        $response->assertJsonCount(10, "data");
     }
 }
