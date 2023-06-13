18875ab830a7ffe2110c912f93cbcfb4a2d02daa
M S <mo.sa963@hotmail.com>
2023-06-13 22:29:44 +0300

    Add 'private' field to playlists table

--- database/migrations/2023_03_30_191620_create_playlists_table.php
+++ database/migrations/2023_03_30_191620_create_playlists_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string("description", 800);
             $table->string("photo")->nullable();
             $table->boolean("album")->default(false);
+            $table->boolean("private")->default(true);
             $table->timestamps();
         });
     }
