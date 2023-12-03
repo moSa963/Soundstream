ea3b3414db22af2f59d18b8943f490f174842ac8
M S <mo.sa963@hotmail.com>
2023-12-03 16:48:37 +0300

    Update react build

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>SoundStream</title>
-    <script type="module" crossorigin src="/assets/index-a63961a1.js"></script>
+    <script type="module" crossorigin src="/assets/index-59b23e8a.js"></script>
     <link rel="stylesheet" href="/assets/index-dcd4892b.css">
   </head>
   <body>
